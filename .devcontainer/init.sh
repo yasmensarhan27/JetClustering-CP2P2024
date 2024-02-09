53b187172a7ecee84757e40c7ece845aa190ec17
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2024-02-09 01:09:35 -0500

    Update init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Add a user with sudo privileges
+useradd -m -s /bin/bash -G sudo codespace
+echo 'codespace ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/codespace
 
+# Switch to the codespace user
+su - codespace
 # Source the environment setup scripts for ROOT CERN, FastJet, and Pythia
 source /root/root/bin/thisroot.sh
 
